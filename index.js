525c884cad7ccee526ce0c5cf81d59694908f076
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-02 00:47:48 -0800

    commit tree 774eae639809b2704145dfe996ecc7aeae7cdcfa

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 /* Your Code Here */
+function createEmployeeRecord(employee) {
+    let record = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return record;
+}
 
+function createEmployeeRecords(employees) {
+    return employees.map(createEmployeeRecord)
+}
+
+function createDSObj(getType, dateStamp) {
+    return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+}
+
+function createTimeInEvent(obj, dateStamp){
+    obj.timeInEvents.push(createDSObj("TimeIn", dateStamp))
+    return obj
+}
+
+function createTimeOutEvent(obj, dateStamp){
+    obj.timeOutEvents.push(createDSObj("TimeOut", dateStamp))
+    return obj
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
